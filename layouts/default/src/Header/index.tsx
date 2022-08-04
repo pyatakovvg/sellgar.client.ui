ce7f9cc8ce3861f153577dd54fda0a759bf8fe57
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-08-04 10:58:00 +0300

    product images

--- layouts/default/src/Header/index.tsx
+++ layouts/default/src/Header/index.tsx
@@ -1,5 +1,5 @@
 
-import { Logotype, Text } from '@library/kit';
+import { Logotype, Header, Text } from '@library/kit';
 import { Widget as BucketWidget } from '@widget/bucket';
 
 import React from 'react';
@@ -8,13 +8,16 @@ import Link from 'next/link';
 import styles from './default.module.scss';
 
 
-function HeaderComponent(): JSX.Element {
+function HeaderComponent() {
   return (
     <section className={styles['wrapper']}>
       <div className={styles['logotype']}>
         <Link href={'/'}>
           <a><Logotype /></a>
         </Link>
+      </div>
+      <div className={styles['header']}>
+        <Header level={4}>Сантехника</Header>
       </div>
       <div className={styles['content']}>
         <Text>тел. <a href={'phone:+79999999999'}>+7 (999) 999-99-99</a></Text>
