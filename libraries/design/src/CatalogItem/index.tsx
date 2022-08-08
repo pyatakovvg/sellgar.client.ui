c8f0730216dd0d1a9ee5827430f754a7cf4ba7ca
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-08-08 21:09:45 +0300

    product item

--- libraries/design/src/CatalogItem/index.tsx
+++ libraries/design/src/CatalogItem/index.tsx
@@ -1,23 +1,28 @@
 
-import { Text } from '@library/kit';
+import { Text, Image } from '@library/kit';
 import { nounDeclension } from '@helper/utils';
 
 import React from 'react';
+import getConfig from 'next/config';
 
 import styles from './@media/index.module.scss';
 
 
 interface IProps {
   name: string;
+  imageUuid: string;
   productsCount: number;
 }
 
+const config = getConfig();
+const process = config['publicRuntimeConfig'];
 
-function Product({ name, productsCount }: IProps) {
+
+function CatalogItem({ name, imageUuid, productsCount }: IProps) {
   return (
     <div className={styles['wrapper']}>
       <div className={styles['image']}>
-
+        <Image src={process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + imageUuid + '?size=160x160'} width={160} height={160} />
       </div>
       <div className={styles['content']}>
         <div className={styles['text']}>
@@ -31,4 +36,4 @@ function Product({ name, productsCount }: IProps) {
   );
 }
 
-export default Product;
+export default CatalogItem;
