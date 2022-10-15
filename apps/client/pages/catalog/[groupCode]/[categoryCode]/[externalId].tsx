5713a7247298912fc385041fe1d5a8f9edc67b81
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-10-15 12:51:09 +0300

    og

--- apps/client/pages/catalog/[groupCode]/[categoryCode]/[externalId].tsx
+++ apps/client/pages/catalog/[groupCode]/[categoryCode]/[externalId].tsx
@@ -26,7 +26,7 @@ export default function ProductByExternalId<NextPage>(props: IProps) {
         <meta property={'og:title'} content={props['data']['title']} />
         <meta property={'og:type'} content={'website'} />
         {props['data']['images'].map((img: any) => (
-          <meta property={'og:image'} content={process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + img['uuid'] + '?width=124&height=124'} />
+          <meta key={img['uuid']} property={'og:image'} content={process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + img['uuid'] + '?width=124&height=124'} />
         ))}
         <meta property="og:image:type" content="image/webp" />
         <meta property="og:image:width" content="124" />
