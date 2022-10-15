02b200e7516936c617933099475a114e20210611
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-10-15 18:03:36 +0300

    gallery

--- modules/product/src/view/Content/index.tsx
+++ modules/product/src/view/Content/index.tsx
@@ -4,7 +4,6 @@ import { Gallery } from '@library/kit';
 import React from 'react';
 import getConfig from 'next/config';
 
-import Image from './Image';
 import Product from './Product';
 
 import styles from './@media/index.module.scss';
@@ -20,12 +19,14 @@ interface IProps {
 }
 
 
-function Content({ data, comments }: IProps): JSX.Element {
+function Content({ data, comments }: IProps) {
   return (
     <section className={styles['wrapper']}>
       <div className={styles['gallery']}>
-        <Gallery type={'large'} width={500} height={500} items={ data['images'].map((src: any) => process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + src['uuid']) } />
-        {/*<Image srcs={ data['images'].map((src: any) => process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + src['uuid']) } />*/}
+        <Gallery
+          type={'large'}
+          items={ data['images'].map((src: any) => process.env['GATEWAY_SERVICE_API'] + '/api/v1/images/' + src['uuid']) }
+        />
       </div>
       <div className={styles['content']}>
         <Product {...data} comments={comments} />
